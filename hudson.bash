ec6f0acb608d51a43804e73cb4088ebfd9f5330a
Owen Nelson <onelson@gmail.com>
2010-11-01 16:54:05 -0400

    adding mysite.demo as the app to be tested to try and focus the coverage report

--- hudson.bash
+++ hudson.bash
@@ -1,2 +1,2 @@
-bin/django test --with-xunit --with-coverage --cover-package=jqhelpers --cover-erase
+bin/django test --with-xunit --with-coverage --cover-package=jqhelpers --cover-erase mysite.demo
 bin/coverage xml --omit="parts,eggs,downloads,mysite"
