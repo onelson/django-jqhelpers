bc11dce72a890953fb2e2387ef0dbc5bfea36e60
Owen Nelson <onelson@gmail.com>
2010-11-01 17:53:15 -0400

    this is the last time I mess with the coverage report

--- hudson.bash
+++ hudson.bash
@@ -1,2 +1,2 @@
-bin/django test --with-xunit --with-coverage --cover-package=jqhelpers --cover-erase
-bin/coverage xml --omit="parts,eggs,downloads,mysite"
+bin/django test --with-xunit --with-coverage --cover-erase --cover-package=jqhelpers
+bin/coverage xml --include="src"
