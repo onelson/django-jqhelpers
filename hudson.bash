b7cf3b30befb9bb091e7204e38d14b5970732ade
Owen Nelson <onelson@gmail.com>
2010-11-10 13:51:50 -0500

    and be quiet while we're at it...

--- hudson.bash
+++ hudson.bash
@@ -1,6 +1,6 @@
 export VE=/tmp/ve/jqhelpers
 source $VE/bin/activate
-pip install -Ur pip/requirements.txt 
+pip install -qUr pip/requirements.txt 
 export PYTHONPATH=.:$PYTHONPATH
 export DJANGO_SETTINGS_MODULE=test_settings
 django-admin.py test
